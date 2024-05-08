543d9fcb45f40699142a5c92c194a9a7f45e3c19
Pranshu <pranshu2.71828@gmail.com>
2024-05-08 21:01:13 +0530

    oops - thought I got that one.

---
+++ api-golang/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"api-golang/database"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	databaseUrl := os.Getenv("DATABASE_URL")
+	if databaseUrl == "" {
+		content, err := ioutil.ReadFile(os.Getenv("DATABASE_URL_FILE"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		databaseUrl = string(content)
+	}
+
+	errDB := database.InitDB(databaseUrl)
+	if errDB != nil {
+		log.Fatalf("⛔ Unable to connect to database: %v\n", errDB)
+	} else {
+		log.Println("DATABASE CONNECTED 🥇")
+	}
+}
+
+func main() {
+	r := gin.Default()
+	var tm time.Time
+
+	r.GET("/", func(c *gin.Context) {
+		tm = database.GetTime(c)
+		c.JSON(200, gin.H{
+			"api":     "golang",
+			"now":     tm,
+			"version": 3,
+		})
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
+		tm = database.GetTime(c)
+		c.JSON(200, "pong")
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (or "PORT" env var)
+}
---
+++ api-node/src/db.js
@@ -0,0 +1,31 @@
+const fs = require("fs");
+
+const { Pool } = require("pg");
+
+databaseUrl =
+  process.env.DATABASE_URL ||
+  fs.readFileSync(process.env.DATABASE_URL_FILE, "utf8");
+
+const pool = new Pool({
+  connectionString: databaseUrl,
+});
+
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
+
+// async/await - check out a client
+const getDateTime = async () => {
+  const client = await pool.connect();
+  try {
+    const res = await client.query("SELECT NOW() as now;");
+    return res.rows[0];
+  } catch (err) {
+    console.log(err.stack);
+  } finally {
+    client.release();
+  }
+};
+
+module.exports = { getDateTime };
